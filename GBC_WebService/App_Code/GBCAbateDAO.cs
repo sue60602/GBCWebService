3f0f8bc23445c3f0468eeef51542908228b1f552
SoA <soa@tmbm.com.tw>
2017-05-09 17:34:29 +0800

    修正回填SQL問題

--- GBC_WebService/App_Code/GBCAbateDAO.cs
+++ GBC_WebService/App_Code/GBCAbateDAO.cs
@@ -11,7 +11,7 @@ namespace GBC_WebService
     public class GBCAbateDAO : GBCAbate_Interface
     {
         private const string DELETE_GBCABATE_STMT =
-        "delete from GBCAbate where PK_會計年度=@PK_會計年度, PK_動支編號=@PK_動支編號, PK_種類=@PK_種類,PK_次別=@PK_次別,PK_明細號@PK_明細號";
+        "delete from GBCAbate where PK_會計年度=@PK_會計年度, PK_動支編號=@PK_動支編號, PK_種類=@PK_種類,PK_次別=@PK_次別,PK_明細號=@PK_明細號";
 
         private const string INSERT_GBCABATE_STMT =
         "insert into GBCAbate values(@PK_會計年度, @PK_動支編號, @PK_種類, @PK_次別, @PK_明細號, @PK_沖銷傳票種類, @PK_沖銷傳票主號, @PK_沖銷傳票細號,  @F_金額, @F_編修狀態, @F_編修人員, @F_編修時間, @F_製票號, @F_製票日期, @F_過帳號, @F_過帳日期)";
---
+++ GBC_WebService/FullVouScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GBC_WebService
+{
+    public class FillVouScript
+    {
+        public string 基金代碼 { get; set; }
+        public string 會計年度 { get; set; }
+        public string 動支編號 { get; set; }
+        public string 種類 { get; set; }
+        public string 次別 { get; set; }
+        public string 明細號 { get; set; }
+        public string 傳票年度 { get; set; }
+        public string 傳票號 { get; set; }
+        public string 傳票明細號 { get; set; }
+        public string 製票日期 { get; set; }
+        //public string 傳票號1 { get; set; }
+        //public string 傳票明細號1 { get; set; }
+        //public string 製票日期1 { get; set; }
+        //public string 傳票號2 { get; set; }
+        //public string 傳票明細號2 { get; set; }
+        //public string 製票日期2 { get; set; }
+    }
+}
