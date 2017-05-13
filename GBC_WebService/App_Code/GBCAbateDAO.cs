6cabdbdadc40d23ff63be6b39e0e42b564a159ae
SoA <soa@tmbm.com.tw>
2017-05-13 19:28:10 +0800

    總之先備份

--- GBC_WebService/App_Code/GBCAbateDAO.cs
+++ GBC_WebService/App_Code/GBCAbateDAO.cs
@@ -11,7 +11,7 @@ namespace GBC_WebService
     public class GBCAbateDAO : GBCAbate_Interface
     {
         private const string DELETE_GBCABATE_STMT =
-        "delete from GBCAbate where PK_會計年度=@PK_會計年度, PK_動支編號=@PK_動支編號, PK_種類=@PK_種類,PK_次別=@PK_次別,PK_明細號=@PK_明細號";
+        "delete from GBCAbate where PK_會計年度=@PK_會計年度 and PK_動支編號=@PK_動支編號 and PK_種類=@PK_種類 and PK_次別=@PK_次別 and PK_明細號=@PK_明細號";
 
         private const string INSERT_GBCABATE_STMT =
         "insert into GBCAbate values(@PK_會計年度, @PK_動支編號, @PK_種類, @PK_次別, @PK_明細號, @PK_沖銷傳票種類, @PK_沖銷傳票主號, @PK_沖銷傳票細號,  @F_金額, @F_編修狀態, @F_編修人員, @F_編修時間, @F_製票號, @F_製票日期, @F_過帳號, @F_過帳日期)";
@@ -52,7 +52,7 @@ namespace GBC_WebService
             com.Parameters.AddWithValue("@PK_沖銷傳票種類", "");
             com.Parameters.AddWithValue("@PK_沖銷傳票主號", "");
             com.Parameters.AddWithValue("@PK_沖銷傳票細號", "");
-            com.Parameters.AddWithValue("@F_金額", gBCAbate.F_金額);
+            com.Parameters.AddWithValue("@F_金額", "");
             com.Parameters.AddWithValue("@F_編修狀態", "");
             com.Parameters.AddWithValue("@F_編修人員", "");
             com.Parameters.AddWithValue("@F_編修時間", "");
